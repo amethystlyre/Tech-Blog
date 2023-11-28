13649fb5815048cfd0a6e4c233bfed70fa2312be
amethystlyre <91407728+amethystlyre@users.noreply.github.com>
2023-11-28 23:07:21 +1100

    update date format in helper

--- utils/helper.js
+++ utils/helper.js
@@ -1,6 +1,11 @@
 module.exports = {
     format_date: (date) => {
-      // Format date as MM/DD/YYYY
-      return date.toLocaleDateString();
+      // Format date as DD MMM YYYY
+      const options = {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      };
+      return date.toLocaleDateString("en-AU", options);
     },
   };
